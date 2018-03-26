13b24161af831d44f8ef712dc88b1f4e5d24b06e
Alexinder Virdee <alex.virdee08@gmail.com>
2018-03-26 17:16:50 -0400

    user model

---
+++ TravelServer/models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// create user Schema
+const UserSchema = new Schema({
+        email: {
+            type: String,
+            required: false
+        },
+        username: { 
+            type: String, 
+            required: true 
+        },
+        encryptedPassword: {
+            type: String,
+            required: true
+        },
+        facebookID: String,
+        googleID: String
+    },
+    {
+        timestamps: true
+    }
+);
+
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
