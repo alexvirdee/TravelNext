88edb4f4f08c5b2a0a7fc9d3f78e6dd2b9135bea
Alexinder Virdee <alex.virdee08@gmail.com>
2018-03-27 10:40:44 -0400

    flight schema

---
+++ TravelServer/models/flights.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// setup schema for flight information
+const FlightSchema = new Schema({
+	 start_location: {
+	 	type: String,
+	 	required: true
+	 },
+	 end_location: {
+	 	type: String,
+	 	required: true
+	 }
+});
+
+const Flight = mongoose.model('Flight', FlightSchema);
+module.exports = Flight;
