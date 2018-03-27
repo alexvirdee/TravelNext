eedee856d1e039732c52e50fe01c603b0392f110
Alexinder Virdee <alex.virdee08@gmail.com>
2018-03-27 11:39:55 -0400

    geometry schema

--- TravelServer/models/country.js
+++ TravelServer/models/country.js
@@ -1,12 +1,26 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+
+// create GEO Location Schema
+const GeoSchema = new Schema({
+	type: {
+		type: String,
+		default: "point"
+	},
+	coordinates: {
+		type: [Number],
+		index: "2dsphere"
+	}
+});
+
 // Country schema
 const CountrySchema = new Schema({
 	name: {
 		type: String,
 		required: true
-	}
+	},
+	geometry: GeoSchema
 });
 
 const Country = mongoose.model('Country', CountrySchema);
