01211e73e1c6cb449f52b741cbdbbc71253eec25
Alexinder Virdee <alex.virdee08@gmail.com>
2018-04-05 15:17:15 -0400

    new components for app

--- TravelClient/src/app/app.module.ts
+++ TravelClient/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { LogoutComponent } from './components/logout/logout.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule
